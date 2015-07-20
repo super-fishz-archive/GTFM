e9b74d170176956396da3b3fb946f12841076142
gyarang <qlqlqkqlqn@gmail.com>
2015-07-21 04:36:20 +0900

    create table문 수정, function문 추가

---
+++ Document/Database/router.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO router(
+            seq, ip, default_gateway, dns_server, subnet_mask, physical_range, 
+            building_seq, room_seq, memo)
+    VALUES (2, '192.168.0.1', '111.123.0.0', '111.123.0.0', '111.123.0.0', '자연과학대', 
+            1, 3, null);
+
