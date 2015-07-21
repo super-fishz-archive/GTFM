660ef4fcd7f0747d4381c84307a293525741f979
kwSeo <tkruddnjs1@naver.com>
2015-07-21 21:24:11 +0900

    build project

--- network-manager/src/test/java/team/gtfm/test/MainTest.java
+++ network-manager/src/test/java/team/gtfm/test/MainTest.java
@@ -1,12 +1,7 @@
 package team.gtfm.test;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class MainTest {
 	public static void main(String[] args){
-		SpringApplication.run(MainTest.class, args);
 	}
 
 }
