acc9cef31056eafa26b17a07d2f06982e6b21b9f
gyarang <qlqlqkqlqn@gmail.com>
2015-07-21 20:48:59 +0900

    cidr형 데이터 text형으로 변경.

--- Document/Database/DB.sql
+++ Document/Database/DB.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.4.4
 -- Dumped by pg_dump version 9.4.4
--- Started on 2015-07-21 20:08:04
+-- Started on 2015-07-21 20:48:20
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -16,7 +16,7 @@ SET client_min_messages = warning;
 SET search_path = public, pg_catalog;
 
 --
--- TOC entry 2086 (class 0 OID 24807)
+-- TOC entry 2086 (class 0 OID 24978)
 -- Dependencies: 173
 -- Data for Name: building; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -35,7 +35,7 @@ SELECT pg_catalog.setval('building_seq_seq', 1, false);
 
 
 --
--- TOC entry 2090 (class 0 OID 24828)
+-- TOC entry 2090 (class 0 OID 24999)
 -- Dependencies: 177
 -- Data for Name: pc; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -60,23 +60,23 @@ SELECT pg_catalog.setval('pc_seq_seq', 1, false);
 
 
 --
--- TOC entry 2096 (class 0 OID 24878)
+-- TOC entry 2096 (class 0 OID 25049)
 -- Dependencies: 183
 -- Data for Name: port; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO port VALUES (1, 1, '100.100.102.101/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', 1, NULL);
-INSERT INTO port VALUES (2, 1, '100.100.102.102/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', 2, NULL);
-INSERT INTO port VALUES (3, 1, '100.100.102.103/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', NULL, NULL);
-INSERT INTO port VALUES (4, 2, '100.100.102.104/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', 3, NULL);
-INSERT INTO port VALUES (5, 2, '100.100.102.105/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', 4, NULL);
-INSERT INTO port VALUES (6, 2, '100.100.102.106/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', NULL, NULL);
-INSERT INTO port VALUES (7, 3, '100.100.102.107/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', 5, NULL);
-INSERT INTO port VALUES (8, 3, '100.100.102.108/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', 6, NULL);
-INSERT INTO port VALUES (9, 3, '100.100.102.109/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', NULL, NULL);
-INSERT INTO port VALUES (10, 4, '100.100.102.110/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', 7, NULL);
-INSERT INTO port VALUES (11, 4, '100.100.102.111/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', 8, NULL);
-INSERT INTO port VALUES (12, 4, '100.100.102.112/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', NULL, NULL);
+INSERT INTO port VALUES (1, 1, '100.100.102.101', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', 1, NULL);
+INSERT INTO port VALUES (2, 1, '100.100.102.102', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', 2, NULL);
+INSERT INTO port VALUES (3, 1, '100.100.102.103', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', NULL, NULL);
+INSERT INTO port VALUES (4, 2, '100.100.102.104', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', 3, NULL);
+INSERT INTO port VALUES (5, 2, '100.100.102.105', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', 4, NULL);
+INSERT INTO port VALUES (6, 2, '100.100.102.106', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', NULL, NULL);
+INSERT INTO port VALUES (7, 3, '100.100.102.107', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', 5, NULL);
+INSERT INTO port VALUES (8, 3, '100.100.102.108', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', 6, NULL);
+INSERT INTO port VALUES (9, 3, '100.100.102.109', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', NULL, NULL);
+INSERT INTO port VALUES (10, 4, '100.100.102.110', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', 7, NULL);
+INSERT INTO port VALUES (11, 4, '100.100.102.111', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', 8, NULL);
+INSERT INTO port VALUES (12, 4, '100.100.102.112', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0', NULL, NULL);
 
 
 --
@@ -89,7 +89,7 @@ SELECT pg_catalog.setval('port_seq_seq', 1, false);
 
 
 --
--- TOC entry 2088 (class 0 OID 24815)
+-- TOC entry 2088 (class 0 OID 24986)
 -- Dependencies: 175
 -- Data for Name: room; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -110,15 +110,15 @@ SELECT pg_catalog.setval('room_seq_seq', 1, false);
 
 
 --
--- TOC entry 2092 (class 0 OID 24841)
+-- TOC entry 2092 (class 0 OID 25012)
 -- Dependencies: 179
 -- Data for Name: router; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO router VALUES (1, '100.100.100.101/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '자연과학대 1층', 1, 1, NULL);
-INSERT INTO router VALUES (2, '100.100.100.102/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '자연과학대 2층', 1, 2, NULL);
-INSERT INTO router VALUES (3, '100.100.100.103/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '상허연구동 1층', 2, 3, NULL);
-INSERT INTO router VALUES (4, '100.100.100.104/32', '255.255.255.0/32', '255.255.255.0/32', '255.255.255.0/32', '상허연구동 2층', 2, 4, NULL);
+INSERT INTO router VALUES (1, '100.100.100.101', '255.255.255.0', '255.255.255.0', '255.255.255.0', '자연과학대 1층', 1, 1, NULL);
+INSERT INTO router VALUES (2, '100.100.100.102', '255.255.255.0', '255.255.255.0', '255.255.255.0', '자연과학대 2층', 1, 2, NULL);
+INSERT INTO router VALUES (3, '100.100.100.103', '255.255.255.0', '255.255.255.0', '255.255.255.0', '상허연구동 1층', 2, 3, NULL);
+INSERT INTO router VALUES (4, '100.100.100.104', '255.255.255.0', '255.255.255.0', '255.255.255.0', '상허연구동 2층', 2, 4, NULL);
 
 
 --
@@ -131,19 +131,19 @@ SELECT pg_catalog.setval('router_seq_seq', 1, false);
 
 
 --
--- TOC entry 2094 (class 0 OID 24862)
+-- TOC entry 2094 (class 0 OID 25033)
 -- Dependencies: 181
 -- Data for Name: switch; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO switch VALUES (1, 1, '100.100.101.101/32', NULL);
-INSERT INTO switch VALUES (2, 1, '100.100.101.102/32', NULL);
-INSERT INTO switch VALUES (3, 2, '100.100.101.103/32', NULL);
-INSERT INTO switch VALUES (4, 2, '100.100.101.104/32', NULL);
-INSERT INTO switch VALUES (5, 3, '100.100.101.105/32', NULL);
-INSERT INTO switch VALUES (6, 3, '100.100.101.106/32', NULL);
-INSERT INTO switch VALUES (7, 4, '100.100.101.107/32', NULL);
-INSERT INTO switch VALUES (8, 4, '100.100.101.108/32', NULL);
+INSERT INTO switch VALUES (1, 1, '100.100.101.101', NULL);
+INSERT INTO switch VALUES (2, 1, '100.100.101.102', NULL);
+INSERT INTO switch VALUES (3, 2, '100.100.101.103', NULL);
+INSERT INTO switch VALUES (4, 2, '100.100.101.104', NULL);
+INSERT INTO switch VALUES (5, 3, '100.100.101.105', NULL);
+INSERT INTO switch VALUES (6, 3, '100.100.101.106', NULL);
+INSERT INTO switch VALUES (7, 4, '100.100.101.107', NULL);
+INSERT INTO switch VALUES (8, 4, '100.100.101.108', NULL);
 
 
 --
@@ -155,7 +155,7 @@ INSERT INTO switch VALUES (8, 4, '100.100.101.108/32', NULL);
 SELECT pg_catalog.setval('switch_seq_seq', 1, false);
 
 
--- Completed on 2015-07-21 20:08:05
+-- Completed on 2015-07-21 20:48:20
 
 --
 -- PostgreSQL database dump complete
